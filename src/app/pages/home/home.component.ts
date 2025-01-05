198c2aa7341cae83db98ca81596f5615b590dc8d
rzeraat.tur <rzeraat.tur@gmail.com>
2025-01-05 05:08:56 +0300

    home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,9 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-    styleUrl: './home.component.scss',
-    standalone: true,
+  styleUrl: './home.component.scss',
+  standalone: true,
 })
 export class HomeComponent {
+  heroUrl = "https://elyoni-pics.s3.eu-north-1.amazonaws.com/products/pexels-photo-288621.webp";
 
+  getHeroUrl() {
+    return `url('${this.heroUrl}') no-repeat center/cover`;
+  }
 }
