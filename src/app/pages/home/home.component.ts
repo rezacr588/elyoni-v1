4575e5b7e264b67fe3d8f043a9bda8258001b5ff
rzeraat.tur <rzeraat.tur@gmail.com>
2025-01-11 05:50:01 +0300

    improve the design

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  styleUrls: ['./home.component.scss'],
   standalone: true,
 })
 export class HomeComponent {
-  heroUrl = "https://elyoni-pics.s3.eu-north-1.amazonaws.com/products/pexels-photo-288621.webp";
+  heroUrl = 'https://elyoni-pics.s3.eu-north-1.amazonaws.com/products/pexels-photo-288621.webp';
 
   getHeroUrl() {
+    // Returns a background image plus 'no-repeat' and 'cover' properties
     return `url('${this.heroUrl}') no-repeat center/cover`;
   }
 }
