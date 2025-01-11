7d4dafe5c6f691bf81c32b022f50fb69173cc0cb
rzeraat.tur <rzeraat.tur@gmail.com>
2025-01-11 06:55:32 +0300

    fix logo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-// Import your page components
+
 import { HomeComponent } from './pages/home/home.component';
-import { ProductsComponent } from './pages/products/products.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
 import { AboutComponent } from './pages/about/about.component';
 import { SourcingComponent } from './pages/sourcing/sourcing.component';
 import { TradeComponent } from './pages/trade/trade.component';
@@ -10,11 +10,11 @@ import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'products', component: ProductsComponent },
+    { path: 'categories', component: CategoriesComponent },
     { path: 'about', component: AboutComponent },
     { path: 'sourcing', component: SourcingComponent },
     { path: 'trade', component: TradeComponent },
     { path: 'blog', component: BlogComponent },
     { path: 'contact', component: ContactComponent },
     { path: '**', redirectTo: '', pathMatch: 'full' },
-  ];
+];
---
+++ src/app/pages/categories/categories.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories',
+  imports: [],
+  templateUrl: './categories.component.html',
+    styleUrl: './categories.component.scss',
+    standalone: true,
+})
+export class CategoriesComponent {}
